15ec3d0a8d1f78cc1ca3080b03c21d343f115d09
modeod <modeod.mdd@gmail.com>
2021-09-27 23:03:00 +0300

    Changed Card Architecture

--- Game1/EnumCards.cs
+++ Game1/EnumCards.cs
@@ -4,29 +4,104 @@ using System.Text;
 
 namespace Game1
 {
-
-    enum Card_Type { Six = 6, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
-
-    enum Card_Suits
+    public enum Card_Suits
     {
-            Hearts,
-            Clubs,
-            Diamonds,
-            Spades
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
     }
-    
-    class Card
+
+    // WTF IDK FOR WHAT THIS IS BUT LET IT BE   . _ .
+    public abstract class DefaultCardDeck
     {
-        private Card_Suits suit;
-        public Card_Suits Suit { get { return suit; } }
+        public readonly Dictionary<string, int> Card_Type;
+        public readonly List<string> Card_Special;
 
-        private Card_Type type;
-        public Card_Type Type { get { return type; } }
+        /// <summary>
+        /// Makes rule for Durak Deck. For example Ace can be beated by Two etc.
+        /// </summary>
+        /// <param name="underBeat">int power = Card_Type[underBeat.typeString] //(2-10, J=11, Q=12, K=13, A=14 </param>
+        public delegate bool RuleForCardBeating(DefaultCard underBeat, DefaultCard beating, Card_Suits trump);
 
-        public Card(Card_Suits suit, Card_Type type)
+        /// <param name="rule"> If rule == null - using basic rules for the game </param>
+        protected static bool IfCardCanBeat(DefaultCard underBeat, DefaultCard beating, Card_Suits trump, RuleForCardBeating rule)
         {
-            this.suit = suit;
-            this.type = type;
+            return rule(underBeat, beating, trump);
+        }
+    }
+
+    public class CardDeck36 : DefaultCardDeck
+    {
+        public readonly static new Dictionary<string, int> Card_Type = new Dictionary<string, int> { { "Six", 6 }, { "Seven", 7 }, { "Eight", 8 }, { "Nine", 9 }, { "Ten", 10 }, { "Jack", 11 }, { "Queen", 12 }, { "King", 13 }, { "Ace", 14 } };
+
+        public new static bool IfCardCanBeat(DefaultCard underBeat, DefaultCard beating, Card_Suits trump, RuleForCardBeating rule)
+        {
+            if(rule == null)
+            {
+                if ((underBeat.suit != trump && beating.suit != trump) || (underBeat.suit == trump && beating.suit == trump))
+                {
+                    if (Card_Type[underBeat.typeString] < Card_Type[beating.typeString])
+                        return true;
+                    else
+                        return false;
+                }
+                else if (underBeat.suit != trump && beating.suit == trump)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return rule(underBeat, beating, trump);
+            }
+        }
+
+    }
+
+    public class CardDeck52 : CardDeck36
+    {
+        public readonly static new Dictionary<string, int> Card_Type = new Dictionary<string, int> { { "Two", 2 }, { "Three", 3 }, { "Four", 4 }, { "Five", 5 }, { "Six", 6 }, { "Seven", 7 }, { "Eight", 8 }, { "Nine", 9 }, { "Ten", 10 }, { "Jack", 11 }, { "Queen", 12 }, { "King", 13 }, { "Ace", 14 } };
+    }
+
+    public class CardDeck54 : CardDeck52
+    {
+        public new readonly List<string> Card_Special = new List<string> { "JokerRed", "JokerBlack" };
+        //TODO: Realise this shit
+    }
+
+    abstract public class DefaultCard
+    {
+        public readonly Card_Suits suit;
+        public readonly int typeInt;
+        public readonly string typeString;
+    }
+
+    public class Card36 : DefaultCard
+    {
+        public readonly Card_Suits suit;
+        public readonly int typeInt;
+        public readonly string typeString;
+
+        public Card36(Card_Suits suit, string type) 
+        {
+            if (!CardDeck36.Card_Type.ContainsKey(type)) { Console.WriteLine("Error: Card36 doesn't have this type of card. Use CardDeck36.CardDeck36"); return; }
+            this.typeString = type;
+            this.typeInt = CardDeck36.Card_Type[type];
+        }
+    }
+
+    public class Card52 : DefaultCard
+    {
+        public readonly Card_Suits suit;
+        public readonly int typeInt;
+        public readonly string typeString;
+
+        public Card52(Card_Suits suit, string type)
+        {
+            if (!CardDeck52.Card_Type.ContainsKey(type)) { Console.WriteLine("Error: Card36 doesn't have this type of card. Use CardDeck36.CardDeck36"); return; }
+            this.typeString = type;
+            this.typeInt = CardDeck36.Card_Type[type];
         }
     }
 }
